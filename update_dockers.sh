8fa06fae3c00592a736306560eea037dfe3dc102
Dacian <dacian@cjtmedia.au>
2023-10-16 14:44:11 +1100

    New update

---
+++ update_dockers.sh
@@ -0,0 +1,27 @@
+ cp -R /home/dacian/docker/dashy/* ./dashy/
+ cp /home/dacian/docker/dokuwiki/docker-compose.yml ./dokuwiki/
+ cp /home/dacian/docker/gitea/docker-compose.yml ./gitea/
+ cp -R /home/dacian/docker/glances/* ./glances/
+ cp /home/dacian/docker/heimdall/docker-compose.yml ./heimdall/
+ cp /home/dacian/docker/homarr/docker-compose.yml ./homarr/
+ cp /home/dacian/docker/jellyfin/docker-compose.yml ./jellyfin/
+ cp /home/dacian/docker/joplin/docker-compose.yml ./joplin/
+ cp /home/dacian/docker/joplin/docker-compose.yml ./joplin/
+ cp /home/dacian/docker/joplin/docker-compose.yml ./joplin/
+ cp /home/dacian/docker/lidarr/docker-compose.yml ./lidarr/
+ cp /home/dacian/docker/mariadb/docker-compose.yml ./mariadb/
+ cp /home/dacian/docker/navidrome/docker-compose.yml ./navidrome/
+ cp /home/dacian/docker/npm/docker-compose.yml ./npm/
+ cp /home/dacian/docker/photoprism/docker-compose.yml ./photoprism/
+ cp /home/dacian/docker/phpmyadmin/docker-compose.yml ./phpmyadmin/
+ cp /home/dacian/docker/prowlarr/docker-compose.yml ./prowlarr/
+ cp /home/dacian/docker/qbittorrent/docker-compose.yml ./qbittorrent/
+ cp /home/dacian/docker/radarr/docker-compose.yml ./radarr/
+ cp /home/dacian/docker/sabnzbd/docker-compose.yml ./sabnzbd/
+ cp /home/dacian/docker/samba-alpine-docker/* ./samba-alpine-docker/
+ cp /home/dacian/docker/sonarr/docker-compose.yml ./sonarr/
+ cp /home/dacian/docker/tig_stack/docker-compose.yml ./tig_stack/
+ cp /home/dacian/docker/tig_stack/docker-compose.yml ./tig_stack/
+ cp /home/dacian/docker/transmission/docker-compose.yml ./transmission/
+ cp /home/dacian/docker/vaultwarden/docker-compose.yml ./vaultwarden/
+ cp /home/dacian/docker/wordpress/docker-compose.yml ./wordpress/
